eac1e0aacc3e3bf4843605e273be03cb8367f74d
Zackary G Tremblay <zackarygtremb@gmail.com>
2022-05-10 17:33:15 -0400

    Squashed commit of the following:
    commit dffb38d54301ae97eb00a71320e9133389d15957
    Author: Zackary G Tremblay <zackarygtremb@gmail.com>
    Date:   Tue May 10 17:32:16 2022 -0400
    
        Update MapComponent.vue
    
    commit 8d06f789254c5b6cff8dbe067a914d31503bccfc
    Author: Zackary G Tremblay <zackarygtremb@gmail.com>
    Date:   Tue May 10 17:18:10 2022 -0400
    
        added map

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <l-map style="height: 500px" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-polyline :lat-lngs="polyline.latlngs" :color="blue"></l-polyline>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LPolyline } from 'vue2-leaflet'
+
+export default {
+  name: 'Map',
+  components: {
+    LMap,
+    LTileLayer,
+    LPolyline
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [46.78601339822761, -71.28621784239412],
+      markerLatLng: [46.78601339822761, -71.28621784239412]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home">
     <ParkListComponent />
+    <MapComponent />
   </div>
 </template>
 
 <script>
+import MapComponent from '../components/MapComponent.vue'
 // @ is an alias to /src
 import ParkListComponent from '../components/ParkListComponent.vue'
 export default {
   name: 'Home',
-  components: { ParkListComponent }
+  components: { MapComponent, ParkListComponent }
 }
 </script>
