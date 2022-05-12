ddf7022bc64f24b0bd14e360291b6cce25940f4a
Zackary G Tremblay <zackarygtremb@gmail.com>
2022-05-12 11:36:54 -0400

    MapComponent finished, waiting for modules

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -2,7 +2,12 @@
   <div>
     <l-map style="height: 500px" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <l-polyline :lat-lngs="polyline.latlngs" :color="blue"></l-polyline>
+      <l-polyline
+        v-for="segment in segments"
+        v-bind:key="segment.id"
+        :lat-lngs="segment.coordinates"
+        :color="changeDifficulty(segment.level)"
+      ></l-polyline>
     </l-map>
   </div>
 </template>
@@ -23,8 +28,36 @@ export default {
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       zoom: 15,
-      center: [46.78601339822761, -71.28621784239412],
-      markerLatLng: [46.78601339822761, -71.28621784239412]
+      center: [46.78601339822761, -71.28621784239412]
+    }
+  },
+  computed: {
+    segments () {
+      return this.$store.state.trail.trailSegments
+    }
+  },
+  methods: {
+    changeDifficulty (difficulty) {
+      this.changeCenter()
+      this.resetZoom()
+      switch (difficulty) {
+        case 'Très Difficile':
+          return 'red'
+        case 'Difficile':
+          return 'orange'
+        case 'Intermédiaire':
+          return 'blue'
+        case 'Facile':
+          return 'green'
+        case 'Inconnue':
+          return 'black'
+      }
+    },
+    resetZoom () {
+      this.zoom = 6
+    },
+    changeCenter () {
+      this.center = this.$store.state.trail.trailSegments[0].coordinates[0]
     }
   }
 }
