e699d5915deb70dbd536b3bf8a75f365a4cf1035
Canadian_Pixel <davetix21@hotmail.com>
2022-05-14 20:31:38 -0400

    Ajout tests service
    -Bug bizarre avec la requete du faut Json mais les test sont bon-ish
    -Il manque les tests d'erreurs

---
+++ src/services/__tests__/parkService.spec.js
@@ -0,0 +1,29 @@
+import mockRequestInterceptor from '@/shared/requestInterceptor'
+import { parkService } from '@/services/parkService'
+import { parksJsonFake } from '@/../tests/data/parksJsonFake'
+
+jest.mock('@/shared/requestInterceptor')
+
+let parks
+
+beforeEach(() => {
+  parks = [...parksJsonFake]
+  jest.clearAllMocks()
+})
+
+describe('parkService.js', () => {
+  test('getAllParksOrderByName retourne une liste trier des parcs', async () => {
+    mockRequestInterceptor.get.mockResolvedValue(parks)
+
+    const response = await parkService.getAllParksOrderByName()
+    expect(response).toEqual(parks)
+  })
+
+  test("getParkById retourne le parc lier à l'id", async () => {
+    const id = 0
+    mockRequestInterceptor.get.mockResolvedValue(parks[id])
+
+    const response = await parkService.getAllParksOrderByName(id)
+    expect(response).toEqual(parks)
+  })
+})
