95af51f3ad03899b281d584f56f160ec4c7289c4
Canadian_Pixel <davetix21@hotmail.com>
2022-05-15 21:21:06 -0400

    Update des fakes pour pas que je meur next time

---
+++ tests/data/likesJsonFake.js
@@ -0,0 +1,7 @@
+export const likesJsonFake = [
+  { userId: 1, trailId: 322, id: 0 },
+  { userId: 1, trailId: 293, id: 1 },
+  { userId: 2, trailId: 375, id: 2 },
+  { userId: 2, trailId: 695, id: 3 }
+]
+// CHANGER LES IDs DE TRAILS!!!!!!!
